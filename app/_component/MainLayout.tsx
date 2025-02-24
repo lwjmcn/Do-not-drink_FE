0ca0662050791a4a912eecc56ad09b216a0529bd
lwjmcn <clarecse02@gmail.com>
2025-02-24 19:50:03 +0900

    feat: responsive main layout

--- app/_component/MainLayout.tsx
+++ app/_component/MainLayout.tsx
@@ -12,8 +12,19 @@ const MainLayout = ({ children }: IMainLayoutProps) => {
   return (
     <AppTheme>
       <CssBaseline enableColorScheme />
-      <Box component="main" sx={{ backgroundColor: "background.default" }}>
-        {children}
+      <Box
+        component="main"
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          maxWidth: { xs: "90%", sm: "90%", md: "90%", lg: "450px" },
+          minHeight: "100vh",
+          marginX: "auto",
+          paddingY: 2,
+          backgroundColor: "background.default",
+        }}
+      >
+        <Box sx={{ flex: 1 }}>{children}</Box>
         <Copyright />
       </Box>
     </AppTheme>
