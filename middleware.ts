fb94ec77ed7d53d18b6917730fb6e64e799c2164
lwjmcn <clarecse02@gmail.com>
2025-02-27 12:48:29 +0900

    feat: private, public routing with auth

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,32 @@
-import NextAuth from "next-auth";
-import { authConfig } from "./auth.config";
+import { NextRequest, NextResponse } from "next/server";
 
-export default NextAuth(authConfig).auth;
+const protectedRoutes = ["/home", "/input", "/history"];
+const publicRoutes = ["/auth"]; // "/"
+const middleware = async (request: NextRequest) => {
+  const accessToken = request.cookies.get("accessToken")?.value;
 
-export const config = {
-  // the protected routes will not even start rendering until the Middleware verifies the authentication
-  matcher: ["/((?!api|_next/static|_next/image|.*\\.png$).*)"],
+  const isAuth = !!accessToken;
+  console.log("isAuth", isAuth);
+
+  // no login: protected -> public
+  if (!isAuth && protectedRoutes.includes(request.nextUrl.pathname)) {
+    return NextResponse.redirect(new URL("/", request.nextUrl.origin));
+  }
+  // login: public -> protected
+  if (
+    isAuth &&
+    (publicRoutes.includes(request.nextUrl.pathname) ||
+      request.nextUrl.pathname === "/")
+  ) {
+    return NextResponse.redirect(new URL("/home", request.nextUrl.origin));
+  }
+
+  return NextResponse.next();
 };
+
+// 특정 경로에서만 middleware 실행
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+};
+
+export default middleware;
