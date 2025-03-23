af705a899c4e13dd4e23737556442c8fa5c09290
lwjmcn <clarecse02@gmail.com>
2025-03-23 19:06:12 +0900

    fix: check exp of jwt

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,25 @@
 import { NextRequest, NextResponse } from "next/server";
+import { jwtDecode } from "jwt-decode";
 
 const protectedRoutes = ["/home", "/input", "/history"];
 const publicRoutes = ["/auth"]; // "/"
 const middleware = async (request: NextRequest) => {
   const accessToken = request.cookies.get("accessToken")?.value;
 
-  const isAuth = !!accessToken;
+  let isAuth = false;
+  if (!accessToken) {
+    console.log("No access token found");
+  } else {
+    const tokenExp = jwtDecode<{ exp?: number }>(accessToken).exp ?? 0;
+    isAuth = tokenExp > Date.now() / 1000;
+
+    console.log(
+      "Token Expiration:",
+      tokenExp,
+      "Current Time:",
+      Date.now() / 1000
+    );
+  }
   console.log("isAuth", isAuth);
 
   // no login: protected -> public
