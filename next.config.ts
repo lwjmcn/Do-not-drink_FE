4b2549ec62d74c4bb33213379feefd63336c9660
lwjmcn <clarecse02@gmail.com>
2025-04-02 21:15:16 +0900

    feat: env config

--- next.config.ts
+++ next.config.ts
@@ -12,6 +12,7 @@ const nextConfig: NextConfig = {
       fs: false,
       path: false,
     };
+
     return config;
   },
   images: {
@@ -19,6 +20,15 @@ const nextConfig: NextConfig = {
   },
   eslint: {
     ignoreDuringBuilds: true,
+  },
+  env: {
+    AUTH_SECRET: process.env.AUTH_SECRET,
+    AUTH_GOOGLE_ID: process.env.AUTH_GOOGLE_ID,
+    AUTH_GOOGLE_SECRET: process.env.AUTH_GOOGLE_SECRET,
+    AUTH_KAKAO_ID: process.env.AUTH_KAKAO_ID,
+    AUTH_KAKAO_SECRET: process.env.AUTH_KAKAO_SECRET,
+    AUTH_URL: process.env.AUTH_URL,
+    NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
   },
 };
 
